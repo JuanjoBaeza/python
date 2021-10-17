e6c0e8aa877a29a38180579bd1f920c2faa78e61
JuanjoBaeza <juanjose.baeza@gmail.com>
2021-10-17 11:44:12 +0200

    Updated scripts

--- bodymass.py
+++ bodymass.py
@@ -1,9 +1,14 @@
 Height=float(input("Enter your height in centimeters: "))
 Weight=float(input("Enter your Weight in Kg: "))
+
 Height = Height/100
+
 BMI=Weight/(Height*Height)
+
 print("your Body Mass Index is: ",BMI)
+
 if(BMI>0):
+
 	if(BMI<=16):
 		print("you are severely underweight")
 	elif(BMI<=18.5):
@@ -13,4 +18,5 @@ if(BMI>0):
 	elif(BMI<=30):
 		print("you are overweight")
 	else: print("you are severely overweight")
+
 else:("enter valid details")
---
+++ clase_simple6.py
@@ -0,0 +1,27 @@
+class Human:
+
+    '''
+    This is a docstring.
+    it is highly recommended that you put here a brief description    
+    of the class
+    '''
+
+    def __init__(self): 
+        self.name = 'Daisy'
+        self.age = '24' 
+        self.gender = 'Women' 
+
+    def listen_to_music(self, song):
+        return "{} is listening {}".format(self.name, song)
+    def running(self, km):
+         return "{} is running {}".format(self.name, km)
+    def sing(self, song):
+        return "{} is singing {}".format(self.name, song)
+    def skate(self):
+        return "{} is skating".format(self.name)
+
+if __name__ == '__main__':
+    per = Human()
+    
+    a = per.listen_to_music('MI_cancion')
+    print (a)
