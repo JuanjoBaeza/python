cf9b32ea17b1b71f7fd6ae0b20de4262003441a2
Juanjo Baeza <juanjose.baeza@gmail.com>
2022-01-23 23:36:35 +0100

    Updated scripts

--- fichero/comparatres.py
+++ fichero/comparatres.py
@@ -8,10 +8,12 @@ def populat(File1, File2, File3):
         Lines2=set(f.readlines())
 
     with open(File3,'a') as f:
-      for line1 in Lines1:
-        for line2 in Lines2:
+
+      for line2 in Lines2:
+        for line1 in Lines1:
           if(line1 == line2):
               f.write(line1)
+
 
 def compare(File1, File2, File3):
 
@@ -24,11 +26,12 @@ def compare(File1, File2, File3):
     with open(File3,'a') as f:
 
         for line in list(e-d):
-           f.write(line)
+          print(line)
+          f.write(line)
 
 File1 = 'fichero/a.txt'
-File2 = 'fichero/b.txt'
+File2 = 'fichero/c.txt'
 File3 = 'fichero/prod.txt'
 
-populat(File1, File2, File3)
+#populat(File1, File2, File3)
 compare(File1, File2, File3)
