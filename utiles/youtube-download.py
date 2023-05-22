23b0399193672000e9efeb4db625025ac437cd90
Juanjo Baeza <juanjose.baeza@gmail.com>
2023-05-22 21:51:58 +0200

    Updated scripts

--- utiles/youtube-download.py
+++ utiles/youtube-download.py
@@ -7,7 +7,7 @@ from pytube import YouTube
 link = input ("Enter the link of YouTube video you want to download: ")
 yt = YouTube(link)
 
-print("Title: ",yt.title)
+#print("Title: ",yt.title)
 # print("Number of views: ",yt.views)
 # print("Length of video: ",yt.length)
 # print("Rating of video: ",yt.rating)
